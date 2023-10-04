854cb4a242a068b59156d6c25d5aeb817eb95aac
= <=>
2023-10-04 12:22:00 +0530

    done password generator useEff,useRef,usecallback

--- 05_password_generator/src/App.jsx
+++ 05_password_generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { useCallback } from "react";
 
@@ -7,20 +7,33 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
-
+const  passwordRef = useRef(null)
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "!@#$%^&*+-_=~`";
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  const copyPassword = useCallback(()=>{
+    passwordRef.current?.select()
+
+window.navigator.clipboard.writeText(password)
+  },[password])
+
+
+  useEffect(() => {
+  passwordGenerator()
+  
+   
+  }, [length , numberAllowed , charAllowed , passwordGenerator])
+  
   return (
     <>
       <div className=" w-full shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 max-w-md mx-auto">
@@ -29,11 +42,55 @@ function App() {
           <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px-3"
+            className="outline-none w-full py-1 px-3 bg-gray-200"
             placeholder="pasword"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button>
+          <button onClick={copyPassword} className="outline-none bg-blue-700 hover:bg-blue-500 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer "
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label className="text-white"> Lenghth: {length} </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+
+            <label htmlFor="numberInput" className="text-white">
+              {" "}
+              Numbers
+            </label>
+          </div>
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput" className="text-white">
+            Characters
+          </label>
         </div>
       </div>
     </>
